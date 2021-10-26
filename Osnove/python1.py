2f9a9c05b02e179ab878a554cba1c2b22e14f34e
Aldin Smajkan <aldinsmajkan2@gmail.com>
2021-10-26 22:15:32 +0200

    First commit (try-out)

---
+++ Osnove/python1.py
@@ -0,0 +1,20 @@
+#                                                【P】 【Y】 【T】 【H】 【O】 【N】                                                      #
+#---------------------------------------------------------------------------------------------------------------------------------------#
+#   U V O D - Za šta se python sve koristi ? 
+#Python se koristi za web development (server-side), software development, matematiku tj. Matematice zadatke i za sistemsko skriptanje.
+#Dakle, moze se koristiti na serveru za kreiranje web aplikacija, moze se koristiti uz software kako bi napravio "workflow"
+#Moguce se povezati na bazu podataka, te istu modifikovati odnosno vrsiti manipulaciju iste, koristi se pri rukovanju sa 
+#velikim podacima i izvršava kompleksne matematičke zadatke.
+#---------------------------------------------------------------------------------------------------------------------------------------#
+
+#---------------------------------------------------------------------------------------------------------------------------------------#
+#  Z A Š T O - Zašto koristit Python?
+# Python podržava dosta sistema odnosno platforme (Windows, Mac, Linux, Raspberry Pi...)
+# Ima jednostavnu sintaksu ~ između ostalog to je razlog što omogućava developerima da napišu određeni program sa manje linija koda u 
+# odnosu na neki drugi programski jezik...
+# Python je interpreterski programski jezik - između ostalog to za nas znači da kod može biti executovan u trenutku kada je i napisan ;
+# Što dalje za nas znači da prototipiranje može biti vrlo brzo ->
+# INTERPRETERSKI JEZICI -> 
+# Kod interpreterskih jezika, program na višem programskom jeziku se prevodi i izvršava instrukcija po instrukcija.
+
+
