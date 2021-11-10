9426c7874d0dc4cff7d64515496af5a3f1640ef7
Aldin Smajkan <aldinsmajkan2@gmail.com>
2021-11-10 20:47:19 +0100

    Booleans

---
+++ Osnove/11_1BooleanPr1.py
@@ -0,0 +1,8 @@
+moj_boolean = True
+print ("Vrijednost varijable moj_boolean je: ", moj_boolean)
+
+print(2 == 3) #Ovdje se upravo vrši ona usporedba vrijednosti
+#Ispisat će se false iz razloga što dva nije jednako 3
+
+print("zdravo" == "zdravo")
+#Ispisat će se true
---
+++ Osnove/11_2BooleanPr2.py
@@ -0,0 +1,7 @@
+x = 7
+
+print(x, "!= 8 :", x != 8)
+print(x, "> 5 :", x > 5)
+print(x, "< 2 :", x < 2)
+print(x, ">= 7 :", x >= 7)
+print(x, "<= 7 :", x <= 7)
