738d3da15f0271c225ea4a3716212ac06dfa7a4d
Aldin Smajkan <aldinsmajkan2@gmail.com>
2021-11-11 09:09:29 +0100

    IF Uslovi

---
+++ ControlFlow/2_1IFEx.py
@@ -0,0 +1,6 @@
+x = 3
+
+if x > 4:
+    print(x, "je veće od 4 i zato se ovo izvršilo")
+    
+print(x, " nije veće od 4 i zato se dio koda unutar if uslova preskočio i izvršio samo ovaj kod")
---
+++ ControlFlow/2_2Nestedifs.py
@@ -0,0 +1,7 @@
+broj = 12
+
+if broj > 5:
+    print("Veće od 5")
+    
+    if broj <= 47:
+        print("Između brojeva 5 i 47")
